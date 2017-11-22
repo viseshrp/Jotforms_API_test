5335420fc95d40ca8402f6c6de4f4c780ea71163
viseshprasad-r <viseshprasad.r@fireeye.com>
2017-11-22 00:48:02 -0500

    Finished basic display

--- js/index.js
+++ js/index.js
@@ -1,21 +1,48 @@
 $(document).ready(function () {
     $('body').bootstrapMaterialDesign();
 
+    $.LoadingOverlay("show");
 
     JF.initialize( {apiKey: "7d7ba832de08daf7ed84f989d65eefc9"} );
 
     JF.getSubmissions(function(response){
-     
-            for(var i=0; i<response.length; i++){
-                var answers_dict = response[i]['answers'];
-                for (var key in answers_dict){
-                    if (answers_dict.hasOwnProperty(key)) {
-                    console.log(answers_dict[key])           
-                        $('#main-cont').append("<div class='card' id='card'><div class='card-body'><h4 class='card-title'>"+answers_dict[key]['name']+"</h4><p class='card-text'>"+""+"</p></div></div><br>");
+
+        if(response!==null) {
+            $.LoadingOverlay("hide");
+                 var j=0;
+                for(var i=0; i<2; i++){
+                    var answers_dict = response[i]['answers'];
+                    
+                    for (var key in answers_dict){
+                        if (answers_dict.hasOwnProperty(key)) {
+                        // console.log(answers_dict[key])           
+                            $('#main-cont').append("<div class='card' id='maincard_"+j+"'><div class='card-body'><p class='card-text'><strong>Name:</strong>&nbsp;&nbsp;<span class='searchcontent' id='cardcontent_"+j+"'>"+answers_dict[key]['name']+"</span></p><p class='card-text'><strong>Text:</strong>&nbsp;&nbsp;"+answers_dict[key]['text']+"</p><p class='card-text'><strong>Type:</strong>&nbsp;&nbsp;"+answers_dict[key]['type']+"</p></div></div><br>");
+                        }
+                        j++;
                     }
                 }
+
             }    
         });
+
+
+        //SEARCH
+    $('#dash_search').on('input', function () {
+        var filter = $(this).val();
+
+        $('.searchcontent').each(function () {
+
+            var div_id = $(this).attr('id').split("content_")[1];
+
+            if ($(this).text().search(new RegExp(filter, "gi")) < 0) {
+                $('#maincard_' + div_id).hide();
+            } else {
+                $('#maincard_' + div_id).show();
+                // console.log(div_id);
+            }
+        });
+    });
+
 
 });
 
